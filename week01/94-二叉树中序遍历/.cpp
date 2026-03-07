c0cbcffd437d9249a6717809b37a573424c820ee
thecake-yzm <hdysospsjzhgdhsoaoshdh577@gmail.com>
2026-03-07 15:37:46 +0800

    Implement inorder traversal for binary tree

---
+++ week01/94-二叉树中序遍历/.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        TreeNode* cur=root;
+        stack<TreeNode*>st;
+        vector<int>lmx;
+        while(cur!=nullptr||!st.empty())
+        {
+            while(cur!=nullptr)
+            {
+                st.push(cur);
+                cur=cur->left;
+            }
+            cur=st.top();
+            st.pop();
+            lmx.push_back(cur->val);
+            cur=cur->right;
+
+        }
+        return lmx;
+    }
+};
